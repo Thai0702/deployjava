f3a7ea12f3fee1b17ec3614affc35d1aa30cf169
Thai0702 <151366056+Thai0702@users.noreply.github.com>
2024-05-18 17:05:23 +0700

    new cômit

--- src/main/java/com/helo/hello/HomeController.java
+++ src/main/java/com/helo/hello/HomeController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class HomeController {
     @GetMapping({"/","/home","/status"})
     public String getStatus(){
-        return  "Hello this is Backend for LVTN Thái Phú";
+        return  "Hello this is Backend";
     }
 
 }
